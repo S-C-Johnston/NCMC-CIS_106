8cd6bc59354f7afdab21b869a1b165c93748beaf
Stewart Johnston <johnstons1@student.ncmich.edu>
2025-03-05 22:39:14 -0500

    Add FormItem.cs

---
+++ src/02/in_class_activity/models/FormItem.cs
@@ -0,0 +1,9 @@
+namespace in_class_activity.models
+{
+    public class FormItem
+    {
+        public string Prompt { get; set; }
+        public string? Response { get; set; }
+        public bool IsOptional { get; set; }
+    }
+};

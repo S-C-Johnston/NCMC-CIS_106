1408f2c9959123d0a4af03da3f7403ddcdd023cd
Stewart Johnston <johnstons1@student.ncmich.edu>
2025-03-06 23:21:01 -0500

    Implement GenerateProgressReport

--- src/02/assignment/services/ProgressReportGenerator.cs
+++ src/02/assignment/services/ProgressReportGenerator.cs
@@ -8,7 +8,26 @@ namespace CIS_106_ASSIGNMENT_2.services
 
         public static void GenerateProgressReport(List<Character> characters, List<Level> levels)
         {
-            // TODO: Write your code here to generate the progress report.
+            string level_prefix = "--";
+            string powerup_prefix = "----";
+            foreach (Character character in characters)
+            {
+                Console.WriteLine($"Progress Report for {character.Name}");
+                foreach (Level level in levels)
+                {
+                    Console.WriteLine("{0}{1} Power Ups Collected:",
+                    level_prefix,
+                    level.Name);
+                    foreach (PowerUp powerUp in level.PowerUps)
+                    {
+                        Console.WriteLine("{0}{1,20}:{2,4}",
+                        powerup_prefix,
+                        powerUp.Name,
+                        character.PowerUps.Contains(powerUp) ? "[X]" : "[ ]"
+                        );
+                    }
+                }
+            }
         }
     }
 

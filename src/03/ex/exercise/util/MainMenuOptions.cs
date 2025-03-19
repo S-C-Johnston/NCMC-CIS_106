65824458174ab7e991892dec2bbcbb1531289228
Stewart Johnston <johnstons1@student.ncmich.edu>
2025-03-18 21:26:34 -0400

    Add MainMenuOptions utility enum

---
+++ src/03/ex/exercise/util/MainMenuOptions.cs
@@ -0,0 +1,11 @@
+/// <summary>
+/// MainMenuOptions is what it says on the tin, but doing it with enums so we're
+/// not checking numbers manually.
+/// </summary>
+public enum MainMenuOptions {
+    Addition,
+    Subtraction,
+    Multiplication,
+    Division,
+    Exit
+}

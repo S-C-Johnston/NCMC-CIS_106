0e7294235e9176ec8d067b6ba7ebe22f8ccfee34
Stewart Johnston <johnstons1@student.ncmich.edu>
2025-03-18 21:34:17 -0400

    Add documentation comment

--- src/03/ex/exercise/util/NumberPrompt.cs
+++ src/03/ex/exercise/util/NumberPrompt.cs
@@ -4,6 +4,15 @@ namespace exercise.util
 {
     public class NumberPrompt
     {
+        /// <summary>
+        /// NumericExtract uses a regular expression to parse an input string
+        /// for that which looks like a number, and returns the first one, or
+        /// null if not found.
+        /// </summary>
+        /// <param name="input">string: hopefully it has a number in it</param>
+        /// <returns>the first number found as a double, or null if not. double?
+        /// as a nullable type can be examined with $_.HasValue and
+        /// $_.Value</returns>
         public static double? NumericExtract(string input)
         {
             Regex numeric_regex = new Regex(@"[-.]?\d+[.]?\d*");

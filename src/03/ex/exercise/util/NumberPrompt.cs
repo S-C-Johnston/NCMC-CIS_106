2e95679891be00846cbaf0442912e3c5fc410a5f
Stewart Johnston <johnstons1@student.ncmich.edu>
2025-03-20 01:01:19 -0400

    Implement PromptForNumber
    A couple false-starts with overcomplication later, this seems like the
    simplest way to get valid input.

--- src/03/ex/exercise/util/NumberPrompt.cs
+++ src/03/ex/exercise/util/NumberPrompt.cs
@@ -1,9 +1,21 @@
-using System.Globalization;
-using System.Text.RegularExpressions;
+using System.Reflection.Metadata.Ecma335;
 
-namespace exercise.util
+namespace exercise.util;
+public class NumberPrompt
 {
-    public class NumberPrompt
+    public double PromptForNumber()
     {
+        bool first_run = false;
+        double result;
+        string maybe_number;
+        do
+        {
+            if (first_run) Console.WriteLine("Numeric input must be valid");
+            first_run = true;
+            Console.Write("number?: ");
+            string? user_input = Console.ReadLine();
+            maybe_number = user_input ?? "";
+        } while (! Double.TryParse(maybe_number, out result));
+        return result;
     }
 }

858418992ead126cc25b31e7ad9c7bbf9ee761dc
Stewart Johnston <johnstons1@student.ncmich.edu>
2025-03-20 00:27:51 -0400

    Remove useless testcases
    I've realized I was barking up the wrong tree. I'm going to leave this
    file here, so I don't have to perform gitromancy to resurrect it, but
    nothing that I'm writing should demand a unit test. I far
    overcomplicated by adding requirements which were not necessary.

--- src/03/ex/exercise.test/NumberPromptTests.cs
+++ src/03/ex/exercise.test/NumberPromptTests.cs
@@ -6,31 +6,4 @@ namespace exercise.test;
 [TestClass]
 public class NumberPromptTests
 {
-    [TestMethod]
-    public void EmptyStringIsNotNumericInput()
-    {
-        string input = "";
-        double? result = NumberPrompt.NumericExtract(input);
-        Assert.IsNull(result);
-    }
-
-    [TestMethod]
-    public void StringsWithNumbersAreValid()
-    {
-        Dictionary<string,Tuple<string,double>> inputs = new()
-        {
-            {"Embedded_Negative", new Tuple<string,double>("asdf-10ghjk", -10)},
-            {"Negative_Floating", new Tuple<string,double>("-3.14",-3.14)},
-            {"Embedded_Floating", new Tuple<string,double>("sentence.42", .42)},
-            {"Phone_Number", new Tuple<string,double>("1-800-555-1234", 1)}
-        };
-
-        foreach (KeyValuePair<string,Tuple<string,double>> input in inputs)
-        {
-            double? result = NumberPrompt.NumericExtract(input.Value.Item1);
-            Assert.IsNotNull(result);
-            Console.WriteLine(result);
-            Assert.AreEqual(input.Value.Item2, result.Value, 1E-14);
-        };
-    }
 }

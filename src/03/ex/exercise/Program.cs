188a559a7060fd7efb8b58e260d67788e41aa0a6
Stewart Johnston <johnstons1@student.ncmich.edu>
2025-03-22 14:38:50 -0400

    Implement main program loop

--- src/03/ex/exercise/Program.cs
+++ src/03/ex/exercise/Program.cs
@@ -1,5 +1,55 @@
 ﻿using exercise.util;
 
 NumberPrompt np = new();
+MainMenu mm = new();
 
-np.PromptForNumber();
+bool exit = false;
+do
+{
+
+    MainMenuOptions mainOperation = mm.PromptMainMenu();
+    if (MainMenuOptions.Exit == mainOperation)
+    {
+        Console.WriteLine("Goodbye!");
+        exit = true;
+        continue;
+    }
+
+    double leftHandOperand = np.PromptForNumber();
+    double rightHandOperand = np.PromptForNumber();
+
+    switch (mainOperation)
+    {
+        case MainMenuOptions.Addition:
+            Console.WriteLine("{0} + {1} = {2}",
+            leftHandOperand,
+            rightHandOperand,
+            (leftHandOperand + rightHandOperand)
+            );
+            break;
+        case MainMenuOptions.Subtraction:
+            Console.WriteLine("{0} - {1} = {2}",
+            leftHandOperand,
+            rightHandOperand,
+            (leftHandOperand - rightHandOperand)
+            );
+            break;
+        case MainMenuOptions.Division:
+            Console.WriteLine("{0} / {1} = {2}",
+            leftHandOperand,
+            rightHandOperand,
+            (leftHandOperand / rightHandOperand)
+            );
+            break;
+        case MainMenuOptions.Multiplication:
+            Console.WriteLine("{0} * {1} = {2}",
+            leftHandOperand,
+            rightHandOperand,
+            (leftHandOperand * rightHandOperand)
+            );
+            break;
+        default:
+            break;
+    }
+
+} while (!exit);

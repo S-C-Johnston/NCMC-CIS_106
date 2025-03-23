0213e3f0da9929a65fef6eaafc1205fccda9ee50
Stewart Johnston <johnstons1@student.ncmich.edu>
2025-03-22 23:23:45 -0400

    Add comments to plot out in Program

--- src/03/asgmt/BookManagement/Program.cs
+++ src/03/asgmt/BookManagement/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+// User options:
+// Add new book to inventory with validation of:
+// - title
+// - author
+// - genre
+// - unique ID
+// List all books
+// Display book by ID
+// Remove book by ID
+// Exit
+
+// The majority of this program should take place within a BookManagerService
+// class. Program.cs should simply instantiate the BookManagerService and start
+// the process.

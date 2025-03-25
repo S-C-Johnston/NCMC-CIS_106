59d90a30f5e0c30f86b1d94d82da77d14f6f84ab
Stewart Johnston <johnstons1@student.ncmich.edu>
2025-03-25 00:27:28 -0400

    Add PromptForBookDetails
    While this can't be said to be a pure function since it relies on the
    state of the object, it doesn't have any destructive side-effects.
    
    I annoyed myself by trying to be terse and clever.
    
    `Console.ReadLine()` only returns `null` under specific circumstances
    using ^Z and <enter>, which I didn't want to try to prompt around.
    Unfortunately, this means that something slick like
    
    ```csharp
    user_input = Console.ReadLine() ?? input_item.Value;
    ```
    
    ...does not work as expected. It compiles, but it's a logical error,
    since an empty line at the CLI will just return an empty string.
    
    I also couldn't think of a readable way to handle something like
    
    ```csharp
    !string.IsNullOrWhiteSpace(Console.ReadLine()) ...
    ```
    
    ...and going on to use that result. I have to cache it somehow. As it
    is, this is more readable. And code is meant to be read, incidentally by
    computers.

--- src/03/asgmt/BookManagement/services/BookManagerService.cs
+++ src/03/asgmt/BookManagement/services/BookManagerService.cs
@@ -36,6 +36,100 @@ public class BookManagerService
         } while (!exit);
     }
 
+    /// <summary>
+    /// PromptForBookDetails writes prompts in a loop. It is deliberatly not
+    /// opinionated except with respect to the uniqueness of an ID, and then
+    /// only because I don't want to litter side effects in the function. It
+    /// depends on the state of the object, however.
+    /// </summary>
+    /// <returns>Book object filled with those details</returns>
+    private Book PromptForBookDetails()
+    {
+        bool previously_run = false;
+        bool valid_int = false;
+        bool valid_ID = false;
+        int input_ID = default;
+        bool user_satisfied = false;
+        Dictionary<string, string> user_inputs = new(){
+            {"Title", ""},
+            {"Author", ""},
+            {"Genre", ""} };
+
+        do
+        {
+            if (previously_run)
+            {
+                if (!valid_int) Console.WriteLine("ID must be valid integer!");
+                if (!valid_ID) Console.WriteLine("ID must not already exist!");
+            }
+
+            string existing_ID = default != input_ID ? $" [{input_ID}]" : "";
+
+            Console.Write("\nEnter the book's integer ID{0}: ", existing_ID);
+            string user_input = Console.ReadLine() ?? "";
+            int this_loop_input_ID = default;
+            valid_int = int.TryParse(user_input, out this_loop_input_ID);
+            if ((default != this_loop_input_ID)
+            && (this_loop_input_ID != input_ID))
+            {
+                input_ID = this_loop_input_ID;
+            }
+            valid_ID = !bookCollection.Keys.Contains(input_ID);
+            if (string.IsNullOrWhiteSpace(user_input) && previously_run)
+            {
+                valid_int = valid_ID;
+            }
+
+            previously_run = true;
+            if (!valid_int || !valid_ID) continue;
+
+            foreach (KeyValuePair<string, string> input_item in user_inputs)
+            {
+                string existing_value
+                = !string.IsNullOrWhiteSpace(input_item.Value)
+                ? $" [{input_item.Value}]"
+                : "";
+
+                Console.Write("\nEnter the book {0}{1}: ",
+                input_item.Key,
+                existing_value);
+
+                user_input = Console.ReadLine() ?? "";
+                user_inputs[input_item.Key] = !string.IsNullOrWhiteSpace(user_input)
+                ? user_input
+                : input_item.Value;
+                // I have decided not to be opinionated about any minimum length
+                // for a Title, Author, or Genre. I print out what they've
+                // written so far for their satisfaction so they can check it.
+                // It's their data.
+            }
+
+            Console.WriteLine("\nYou entered...");
+            Console.WriteLine("{0,10} : {1}", "ID", input_ID);
+            foreach (KeyValuePair<string, string> input_item in user_inputs)
+            {
+                Console.WriteLine($"{input_item.Key,10} : {input_item.Value}");
+            }
+            Console.Write("Is all this correct? [y]/n: ");
+            string satisfaction_prompt = Console.ReadLine() ?? "";
+            if (!string.IsNullOrWhiteSpace(satisfaction_prompt))
+            {
+                user_satisfied = string.Equals($"{satisfaction_prompt.ToLower().First()}", "y");
+            }
+            else
+            {
+                user_satisfied = true;
+            }
+        } while (!user_satisfied);
+
+        return new Book{
+            Title = user_inputs["Title"],
+            Author = user_inputs["Author"],
+            Genre = user_inputs["Genre"],
+            ID = input_ID
+        };
+    }
+
     private Dictionary<BookManagementMenuItems, string> MenuItemExplanatory = new() {
             { BookManagementMenuItems.Add, "Add a book to the collection" },
             { BookManagementMenuItems.List, "List all books from the collection" },

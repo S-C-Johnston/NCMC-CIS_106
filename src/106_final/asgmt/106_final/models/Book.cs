93e9c76813fc1415342b4e49e7ccbda2c5f99e70
Stewart Johnston <johnstons1@student.ncmich.edu>
2025-05-10 19:22:56 -0400

    Add TODO for book.cs
    In the middle of rewriting other services, I'm realizing, my data model
    is weird. Why the hell was I implementing this all with an int book ID
    instead of letting the machine handle it with a List<book>? It makes
    everything I'm doing so much more complicated. Was I thinking about
    ISBNs? Was I being cheeky that way? Idk, but this is genuinely not that
    useful so I might rip it out and see how my thinking changes.

--- src/106_final/asgmt/106_final/models/Book.cs
+++ src/106_final/asgmt/106_final/models/Book.cs
@@ -13,5 +13,9 @@ public class Book
 public required string Title { get; set; }
 public required string Author { get; set; }
 public required string Genre { get; set; }
+// TODO: Evaluate the utility of this? I think I was thinking, at the time I
+// wrote the original code, that this would be easier to return as a whole
+// object. I don't remember why I thought that. Maybe my thinking was like "ISBN
+// is a thing, so each book should have an ID".
 public required int ID { get; set; }
 }

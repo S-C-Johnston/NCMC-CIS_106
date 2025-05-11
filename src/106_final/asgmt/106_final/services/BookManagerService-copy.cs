5e04dcdca25c88f11c55ae2084822e0b4733fb77
Stewart Johnston <johnstons1@student.ncmich.edu>
2025-05-11 19:57:07 -0400

    Replace datatype of collection, add CRUD ops

--- src/106_final/asgmt/106_final/services/BookManagerService-copy.cs
+++ src/106_final/asgmt/106_final/services/BookManagerService-copy.cs
@@ -3,14 +3,12 @@ using final.Models;
 namespace final.Services;
 
 /// <summary>
-/// BookManagerService is meant to be instantiated and run with .Dispatch().
-/// It's rather a program in and of itself. The object tracks state internally,
-/// and does not export it in any way.
+/// BookService is meant to be a source of truth data source of Books
 /// </summary>
-public class BookManagerService
+public static class BookService
 {
 
-    private Dictionary<int, Book> bookCollection = new();
+    private static List<Book> bookCollection = new();
 
     // I must implement the following:
     // Create: Add, "Add a book to the collection"
@@ -18,4 +16,95 @@ public class BookManagerService
     // Read: Display, "Display information about a book by ID"
     // Update: Edit, "Change details about the book"
     // Delete: Remove, "Remove a book by ID"
+
+    /// <summary>
+    /// AddBookRecord does what it says on the tin, checking for duplicates.
+    /// </summary>
+    /// <param name="book"></param>
+    /// <returns>false if the book already exists</returns>
+    public static bool AddBookRecord(Book book)
+    {
+        if (CheckForBook(book))
+        {
+            return false;
+        }
+        bookCollection.Add(book);
+        return true;
+    }
+
+    /// <summary>
+    /// GetAll does what it says on the tin.
+    /// </summary>
+    public static List<Book> GetAll()
+    {
+        return bookCollection;
+    }
+
+    /// <summary>
+    /// GetBookByIndex accepts a numeric ID and a callback exterior variable. It
+    /// returns a bool, and the exterior value is modified if applicable.
+    /// </summary>
+    /// <returns>false if the book was not found</returns>
+    public static bool GetBookByIndex(int inputID, out Book? book)
+    {
+        try
+        {
+            book = bookCollection[inputID];
+        }
+        catch (ArgumentOutOfRangeException)
+        {
+            book = null;
+            return false;
+        }
+        return true;
+    }
+
+    /// <summary>
+    /// CheckForBook does what it says on the tin
+    /// </summary>
+    /// <param name="book">Book whose existence is checked for</param>
+    /// <returns>bool condition of if the book input exists in the collection</returns>
+    private static bool CheckForBook(Book book)
+    {
+        return bookCollection.Exists( b => b == book);
+    }
+
+    /// <summary>
+    /// ReplaceBookRecord does what it says on the tin. The whole object at the
+    /// given index is replaced.
+    /// </summary>
+    /// <param name="index"></param>
+    /// <param name="book"></param>
+    /// <returns></returns>
+    public static bool ReplaceBookRecord(int index, Book book)
+    {
+        try
+        {
+           bookCollection[index] = book;
+        }
+        catch (IndexOutOfRangeException)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    /// <summary>
+    /// RemoveSingleBookRecord takes a numeric ID and removes the corresponding
+    /// record, if any.
+    /// </summary>
+    /// <returns>false if the book was not found</returns>
+    public static bool RemoveSingleBookRecord(int remove_book_ID)
+    {
+        try
+        {
+            bookCollection.RemoveAt(remove_book_ID);
+        }
+        catch (ArgumentOutOfRangeException)
+        {
+            return false;
+        }
+        return true;
+    }
+
 }

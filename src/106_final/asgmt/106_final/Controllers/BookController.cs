f2912bec3cd72aa13043d965d6544e4a12a564ee
Stewart Johnston <johnstons1@student.ncmich.edu>
2025-05-11 21:04:29 -0400

    Implement basic Read verbs

--- src/106_final/asgmt/106_final/Controllers/BookController.cs
+++ src/106_final/asgmt/106_final/Controllers/BookController.cs
@@ -13,7 +13,37 @@ public class BookController : ControllerBase
     }
 
     // GET all
+    /// <summary>
+    /// GetAll does what it says on the tin, but returns the results as a sorted
+    /// list with the index as the key
+    /// </summary>
+    /// <returns>SortedList<int,Book> where the index is the key</returns>
+    [HttpGet]
+    public ActionResult<SortedList<int,Book>> GetAll()
+    {
+        return new SortedList<int, Book>(
+         BookService.GetAll()
+         .Select((book, index) => new { index, book })
+         .ToDictionary(
+             selection => selection.index,
+             selection => selection.book));
+    }
+
     // GET individual
+    /// <summary>
+    /// Get takes an integer id, which is the index of the item in the list.
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns>Book record</returns>
+    [HttpGet("{id:int}")]
+    public ActionResult<Book> Get(int id)
+    {
+        Book? book;
+        BookService.GetBookByIndex(id, out book);
+        if(null == book) return NotFound();
+        return book;
+    }
+
     // POST
     // PUT replace individual
     // DELETE individual

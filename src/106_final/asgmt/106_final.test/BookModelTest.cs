0280558e883049e5e296e566563ac884879673e8
Stewart Johnston <johnstons1@student.ncmich.edu>
2025-05-12 10:08:12 -0400

    Add tests for BookModel, particularly equality

--- src/106_final/asgmt/106_final.test/BookModelTest.cs
+++ src/106_final/asgmt/106_final.test/BookModelTest.cs
@@ -1,10 +1,41 @@
-namespace _106_final.test;
+using final.Models;
+namespace final.test;
 
 [TestClass]
-public class UnitTest1
+public class BookModelTests
 {
+        private Book? null_book = null;
+        private Book filled_book = new Book{
+            Title = "string",
+            Author = "string",
+            Genre = "string"
+        };
+        private Book another_book = new Book{
+            Title = "string",
+            Author = "string",
+            Genre = "string"
+        };
+        private Book hhg = new Book{
+            Title = "HitchHiker's Guide",
+            Author = "Douglas Adams",
+            Genre = "Sci-Fi,Comedy"
+        };
+
     [TestMethod]
-    public void TestMethod1()
+    public void BookEquality_Null_ReturnFalse()
     {
+        Assert.AreNotEqual(null_book, filled_book);
+    }
+
+    [TestMethod]
+    public void BookEquality_Plain_ReturnTrue()
+    {
+        Assert.AreEqual(filled_book,another_book);
+    }
+
+    [TestMethod]
+    public void BookEquality_DiffBooks_ReturnFalse()
+    {
+        Assert.AreNotEqual(filled_book,hhg);
     }
 }

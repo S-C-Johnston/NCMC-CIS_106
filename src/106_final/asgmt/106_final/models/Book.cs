5678b16b8b85b728ca9ed563a310bd01ac9bf7b8
Stewart Johnston <johnstons1@student.ncmich.edu>
2025-05-11 23:04:49 -0400

    Implement IEquatable
    Even tho Book is barely more than a container of values, it needs
    equality defined so we can check for duplicates.

--- src/106_final/asgmt/106_final/models/Book.cs
+++ src/106_final/asgmt/106_final/models/Book.cs
@@ -8,9 +8,51 @@ namespace final.Models;
 /// of state. If it were smaller, and of fixed size, it'd be a candidate for a
 /// struct.
 /// </remarks>
-public class Book
+public class Book : IEquatable<Book>
 {
 public required string Title { get; set; }
 public required string Author { get; set; }
 public required string Genre { get; set; }
+
+    public override bool Equals(object? obj)
+    {
+        //
+        // See the full list of guidelines at
+        //   http://go.microsoft.com/fwlink/?LinkID=85237
+        // and also the guidance for operator== at
+        //   http://go.microsoft.com/fwlink/?LinkId=85238
+        //
+
+        if (obj == null || GetType() != obj.GetType())
+        {
+            return false;
+        }
+        else
+        {
+            return this.Equals((Book)obj);
+        }
+    }
+    public bool Equals(Book? book)
+    {
+        if (book is null) return false;
+        return this.Author == book.Author &&
+        this.Title == book.Title &&
+        this.Genre == book.Genre;
+    }
+
+    public override int GetHashCode() => (Title, Author, Genre).GetHashCode();
+
+    public static bool operator ==(Book? lhs, Book? rhs)
+    {
+        if (lhs is null) {
+            if (rhs is null)
+            {
+                return true;
+            }
+            return false;
+        }
+        return lhs.Equals(rhs);
+    }
+    public static bool operator !=(Book lhs, Book rhs) => !(lhs == rhs);
+
 }

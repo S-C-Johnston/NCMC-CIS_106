1ab54fef2902cde8ad907b37d79e71776b3fd5e1
Stewart Johnston <johnstons1@student.ncmich.edu>
2025-05-11 20:27:04 -0400

    Add skeleton of BookController

---
+++ src/106_final/asgmt/106_final/Controllers/BookController.cs
@@ -0,0 +1,20 @@
+using final.Models;
+using final.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace final.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class BookController : ControllerBase
+{
+    public BookController()
+    {
+    }
+
+    // GET all
+    // GET individual
+    // POST
+    // PUT replace individual
+    // DELETE individual
+}
